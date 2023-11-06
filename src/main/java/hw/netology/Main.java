bf6d671a28aeadf633d2652881b49b8bb295c4e4
AnF <anfwork007@gmail.com>
2023-11-06 19:58:27 +0300

    initial commit

---
+++ src/main/java/hw/netology/Main.java
@@ -0,0 +1,8 @@
+package hw.netology;
+
+public class Main {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+        System.out.println("Hello and welcome!");
+        }
+    }
